3f5095f5000bb76ff66cb2df6af234034ebf93f9
LindaSmalka <linda.smalkaa@gmail.com>
2022-10-30 13:17:46 +0200

    Update LoopHomeWork.java

--- src/main/java/lekcijaTris/majasDarbs/LoopHomeWork.java
+++ src/main/java/lekcijaTris/majasDarbs/LoopHomeWork.java
@@ -7,48 +7,151 @@ public class LoopHomeWork {
     public static void main(String[] args) {
 
         uzdevumsViens();
+        uzdevumsDivi();
+        uzdevumsTris();
+
 
 
     }
 
 
-        public static void uzdevumsViens () {
+    public static void uzdevumsViens() {
 
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            int skaitlis;
-            int summa = 0;
-            while (summa < 100) {
+        int skaitlis;
+        int summa = 0;
+        while (summa < 100) {
 
-                System.out.println("Ievadi skaitli:");
-                skaitlis = scanner.nextInt();
-                summa = summa + skaitlis;
+            System.out.println("Ievadi skaitli:");
+            skaitlis = scanner.nextInt();
+            summa = summa + skaitlis;
 
-            }
-            System.out.println("Gatavs");
+        }
+        System.out.println("Gatavs");
 
+
+    }
+
+    public static void uzdevumsDivi() {
+
+        //while
+
+        System.out.println("Piemēri ar WHILE");
+
+        int[] izdevumi = {89, 95, 150, 63, 195};
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Pēdējo mēnešu izdevumi " + izdevumi[i]);
+            i++;
+        }
+
+        int i1 = 0;
+        while (i1 < izdevumi.length) {
+            System.out.println(izdevumi[i1]);
+            i1 = i1 + 1;
+        }
+
+        String[] valstis = {"Igaunija", "Norvēģija", "Polija", "Portugāle"};
+        int v = 0;
+        while (v < 4) {
+            System.out.println("Šogad apceļotās valstis " + valstis[v]);
+            v++;
 
         }
 
-    public static void uzdevumsDivi () {
+        char[] burti = {'a', 'b', 'c', 'd', 'e',};
+        int b = 0;
+        while (b < 5) {
+            System.out.println("Pirmie alfabēta burti " + burti[b]);
+            b = b + 1;
 
-        //int
-        //String
-        //char
+        }
 
+        //do while
 
+        System.out.println("Piemēri ar DO WHILE");
+
+        int i2 = 0;
+        do {
+            System.out.println("Izdevumi");
+            i2++;
+        } while (i2 < izdevumi.length);
+
+        int v1 = 0;
+        do {
+            System.out.println(valstis[v1]);
+            v1++;
+        }
+        while (v1 < valstis.length);
+
+        int b1 = 0;
+        do {
+            System.out.println(burti[b1]);
+            b1++;
+        }
+        while (b1 < 3);
+
+        //for loop
+
+        System.out.println("Piemēri ar FOR");
+
+        for (int u = 0; u < izdevumi.length; u++) {
+            System.out.println(izdevumi[u]);
+        }
+
+        for (int u = 0; u < 4; u++) {
+            System.out.println(valstis[u]);
+        }
+
+        for (int u = 0; u < burti.length; u++) {
+            System.out.println(burti[u]);
+        }
+
+        //for each
+
+        System.out.println("Piemēri ar FOR EACH");
+
+        for (int mēnešaIzdevumi : izdevumi) {
+            System.out.println(mēnešaIzdevumi);
+        }
+
+        for (String valstuNosaukumi : valstis) {
+            System.out.println(valstuNosaukumi);
+        }
+
+        for (char alfabētaBurti : burti) {
+            System.out.println(alfabētaBurti);
+        }
     }
 
-    public static void uzdevumsTris () {
+    public static void uzdevumsTris() {
 
 
+
+//
+        int[] masivs =  new int[100];
+        for (int i = 0; i < masivs.length; i++) {
+            masivs[i] = i;
+        }
+        for (int j: masivs){
+        if (j%2==0) {
+            System.out.println("Pāra skaitļi:" + j);
+        }
+
+        }
     }
 
-    public static void uzdevumsCetri () {
 
-
+    public static void uzdevumsCetri() {
     }
 
+        }
 
-    }
+
+
+
+
+
+
 
